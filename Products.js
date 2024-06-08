a9f2b2368cd7b9ad1433e43ee6780dfe640e1700
Ilyas-Taj07 <ilyas.hussain.t@gmail.com>
2024-06-08 22:31:43 +0530

    Added Products

---
+++ Products.js
@@ -0,0 +1,240 @@
+
+/*
+
+Types
+
+1. Accessories
+2. Mens
+3. Womens
+4. Shoes
+
+*/
+
+
+let Products = [
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Accessories`
+    // },
+    // {
+    //     id: 1,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Accessories`
+    // },
+    // {
+    //     id: 2,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Accessories`
+    // },
+    // {
+    //     id: 3,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Accessories`
+    // },
+    // {
+    //     id: 4,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Accessories`
+    // },
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Mens`
+    // },
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Mens`
+    // },
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Mens`
+    // },
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Mens`
+    // },
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Mens`
+    // },
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Womens`
+    // },
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Womens`
+    // },
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Womens`
+    // },
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Womens`
+    // },
+    // {
+    //     id: 0,
+    //     brand: ``,
+    //     title: ``,
+    //     price: 0,
+    //     actualPrice: 0,
+    //     rating: ,
+    //     reviews: ,
+    //     imgurl: `https://icubecode-server.onrender.com/icubecode/`,
+    //     type: `Womens`
+    // },
+    {
+        id: 0,
+        brand: `adidas`,
+        title: `Adidas Mens Throb M CBLACK/FTWWHT Running Shoe - 8 UK (CM4885)`,
+        price: `1,922`,
+        actualPrice: `2,699`,
+        rating: 4.1,
+        reviews: 612,
+        imgurl: `https://icubecode-server.onrender.com/icubecode/Adidas_Mens_Throb.jpg`,
+        type: `Shoes`
+    },
+    {
+        id: 1,
+        brand: `adidas`,
+        title: `adidas Mens Astoundrun M Running Shoe`,
+        price: `2,182`,
+        actualPrice: `3,799`,
+        rating: 4.0,
+        reviews: 494,
+        imgurl: `https://icubecode-server.onrender.com/icubecode/adidas_Mens_Astoundrun.png`,
+        type: `Shoes`
+    },
+    {
+        id: 2,
+        brand: `adidas`,
+        title: `adidas Mens Drogo M Running Shoe`,
+        price: `1,769`,
+        actualPrice: `2,999`,
+        rating: 4.0,
+        reviews: 13477,
+        imgurl: `https://icubecode-server.onrender.com/icubecode/adidas_Mens_Drogo.png`,
+        type: `Shoes`
+    },
+    {
+        id: 3,
+        brand: `adidas`,
+        title: `adidas Men's Adivat M Running Shoes`,
+        price: `1,727`,
+        actualPrice: `2,699`,
+        rating: 4.0,
+        reviews: 3041,
+        imgurl: `https://icubecode-server.onrender.com/icubecode/adidas_Mens_Adivat.png`,
+        type: `Shoes`
+    },
+    {
+        id: 4,
+        brand: `Bata`,
+        title: `Bata Mens I-and Laceup Casual Shoes`,
+        price: `1,212`,
+        actualPrice: `1,799`,
+        rating: 3.8,
+        reviews: 407,
+        imgurl: `https://icubecode-server.onrender.com/icubecode/Bata_Mens_I.png`,
+        type: `Shoes`
+    }
+
+]
+
+
+
+module.exports = { Products }
--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const cors = require('cors')
 const app = express()
 
 const { users } = require('./Users')
+const { Products } = require('./Products')
 
 app.use(cors())
 app.use(express.json())
-
+app.use('/icubecode', express.static('images'))
 
 app.get('/', (req, res) => {
     res.send("<div style='text-align:center'><h1>iCube Code</h1></div>")
@@ -41,11 +42,39 @@ app.get('/users', (req, res) => {
 
 })
 
+app.get('/products', (req, res) => {
+
+    let values = Products.filter((item) => {
+        console.log(item)
+        let id = req.query['id'] !== undefined ?
+            item.id.toString().includes(req.query['id'])
+            : true
+
+        let type = req.query['type'] !== undefined ?
+            item.type.toLowerCase().includes(req.query['type'].toLowerCase())
+            : true
+
+        return id && type
+
+    })
+
+    res.json(values)
+
+})
+
 app.get('/users/:id', (req, res) => {
 
     const { id } = req.params
 
     res.json(users.filter(item => item.id === Number.parseInt(id)))
+
+})
+
+app.get('/products/:id', (req, res) => {
+
+    const { id } = req.params
+
+    res.json(Products.filter(item => item.id === Number.parseInt(id)))
 
 })
 
