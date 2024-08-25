71d23c210cbb1310a3cd22b378dc6c91a995fe64
Ilyas-Taj07 <ilyas.hussain.t@gmail.com>
2024-08-25 22:13:29 +0530

    Added DOB to the users data

--- Users.js
+++ Users.js
@@ -3,181 +3,211 @@ let users = [
         id: 1843,
         username: "zoro",
         email: "zoro1542@icubecode.com",
-        phoneNumber: 6825975684
+        phoneNumber: 6825975684,
+        dob: '02-09-1999'
     },
     {
         id: 1844,
         username: "kareena",
         email: "kareena@icubecode.com",
-        phoneNumber: 7859425963
+        phoneNumber: 7859425963,
+        dob: '18-01-1999'
     },
     {
         id: 1845,
         username: "nami",
         email: "nami787@icubecode.com",
-        phoneNumber: 9867598475
+        phoneNumber: 9867598475,
+        dob: '27-11-1998'
     },
     {
         id: 1846,
         username: "john42",
         email: "john42@icubecode.com",
-        phoneNumber: 6425891250
+        phoneNumber: 6425891250,
+        dob: '15-06-1999'
     },
     {
         id: 1847,
         username: "jordan",
         email: "jordan@icubecode.com",
-        phoneNumber: 7001694500
+        phoneNumber: 7001694500,
+        dob: '23-08-1999'
     },
     {
         id: 1849,
         username: "stark",
         email: "stark@icubecode.com",
-        phoneNumber: 6100892521
+        phoneNumber: 6100892521,
+        dob: '18-02-1999'
     },
     {
         id: 1850,
         username: "steve",
         email: "steve@icubecode.com",
-        phoneNumber: 7801128677
+        phoneNumber: 7801128677,
+        dob: '20-10-1999'
     },
     {
         id: 1851,
         username: "quinn",
         email: "quinn@icubecode.com",
-        phoneNumber: 7569589574
+        phoneNumber: 7569589574,
+        dob: '07-10-1999'
     },
     {
         id: 1852,
         username: "nevaeh",
         email: "nevaeh@icubecode.com",
-        phoneNumber: 8957253642
+        phoneNumber: 8957253642,
+        dob: '23-10-1996'
     },
     {
         id: 1853,
         username: "piper",
         email: "piper@icubecode.com",
-        phoneNumber: 7685954785
+        phoneNumber: 7685954785,
+        dob: '19-11-1998'
     },
     {
         id: 1854,
         username: "ruby",
         email: "ruby@icubecode.com",
-        phoneNumber: 9684576325
+        phoneNumber: 9684576325,
+        dob: '11-03-1998'
     },
     {
         id: 1855,
         username: "willow",
         email: "willow@icubecode.com",
-        phoneNumber: 7005631402
+        phoneNumber: 7005631402,
+        dob: '09-03-1997'
     },
     {
         id: 1856,
         username: "cora",
         email: "cora@icubecode.com",
-        phoneNumber: 9110578547
+        phoneNumber: 9110578547,
+        dob: '21-01-1998'
     },
     {
         id: 1857,
         username: "kaylee",
         email: "kaylee@icubecode.com",
-        phoneNumber: 9725869100
+        phoneNumber: 9725869100,
+        dob: '31-01-1992'
     },
     {
         id: 1858,
         username: "eliana",
         email: "eliana@icubecode.com",
-        phoneNumber: 9001275863
+        phoneNumber: 9001275863,
+        dob: '17-09-1999'
     },
     {
         id: 1859,
         username: "pedro",
         email: "pedro@icubecode.com",
-        phoneNumber: 8112795699
+        phoneNumber: 8112795699,
+        dob: '04-04-1974'
     },
     {
         id: 1860,
         username: "melanie",
         email: "melanie@icubecode.com",
-        phoneNumber: 6100458938
+        phoneNumber: 6100458938,
+        dob: '02-06-1976'
     },
     {
         id: 1861,
         username: "gianna",
         email: "gianna@icubecode.com",
-        phoneNumber: 7788100258
+        phoneNumber: 7788100258,
+        dob: '20-12-1971'
     },
     {
         id: 1862,
         username: "julia",
         email: "julia@icubecode.com",
-        phoneNumber: 6681008966
+        phoneNumber: 6681008966,
+        dob: '01-01-1992'
     },
     {
         id: 1863,
         username: "valentina",
         email: "valentina@icubecode.com",
-        phoneNumber: 8988675723
+        phoneNumber: 8988675723,
+        dob: '19-02-1994'
     },
     {
         id: 1864,
         username: "nova",
         email: "nova@icubecode.com",
-        phoneNumber: 7689613583
+        phoneNumber: 7689613583,
+        dob: '02-01-1984'
     },
     {
         id: 1865,
         username: "clara",
         email: "clara@icubecode.com",
-        phoneNumber: 6325684001
+        phoneNumber: 6325684001,
+        dob: '22-12-1988'
     },
     {
         id: 1866,
         username: "vivian",
         email: "vivian@icubecode.com",
-        phoneNumber: 7649226898
+        phoneNumber: 7649226898,
+        dob: '15-06-1992'
     },
     {
         id: 1867,
         username: "mackenzie",
         email: "mackenzie@icubecode.com",
-        phoneNumber: 6823003201
+        phoneNumber: 6823003201,
+        dob: '20-09-1992'
     },
     {
         id: 1868,
         username: "madeline",
         email: "madeline@icubecode.com",
-        phoneNumber: 9586841258
+        phoneNumber: 9586841258,
+        dob: '17-10-1996'
     },
     {
         id: 1869,
         username: "rylee",
         email: "rylee@icubecode.com",
-        phoneNumber: 7695586656
+        phoneNumber: 7695586656,
+        dob: '02-08-1978'
     },
     {
         id: 1870,
         username: "taylor",
         email: "taylor@icubecode.com",
-        phoneNumber: 6922002460
+        phoneNumber: 6922002460,
+        dob: '11-05-1994'
     },
     {
         id: 1871,
         username: "mary",
         email: "mary@icubecode.com",
-        phoneNumber: 9534002456
+        phoneNumber: 9534002456,
+        dob: '01-11-1998'
     },
     {
         id: 1872,
         username: "natalia",
         email: "natalia@icubecode.com",
-        phoneNumber: 769455286
+        phoneNumber: 769455286,
+        dob: '02-12-1999'
     },
     {
         id: 1873,
         username: "arya",
         email: "arya@icubecode.com",
-        phoneNumber: 6789579576
+        phoneNumber: 6789579576,
+        dob: '27-04-1971'
     }
 ]
 
